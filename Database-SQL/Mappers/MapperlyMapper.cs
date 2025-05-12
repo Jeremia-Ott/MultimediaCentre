50d68729ec59254ee9b11cf1be8fc5d0ef538d0a
Jeremia Ott <jeremia.ott@edu.fh-joanneum.at>
2025-05-12 21:39:41 +0200

    Add mapper

---
+++ Database-SQL/Mappers/MapperlyMapper.cs
@@ -0,0 +1,10 @@
+﻿using Riok.Mapperly.Abstractions;
+using Shared.Models;
+
+namespace Database_SQL.Mappers;
+
+[Mapper]
+public partial class MapperlyMapper
+{
+    public partial Animemovie Map(Animemovie animemovie);   // ToDo
+}
